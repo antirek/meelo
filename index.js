85f7928d5be26887ffed8f079ef697a466b4c1fa
antirek <serge.dmitriev@gmail.com>
2015-07-27 23:00:55 +0700

    add q promise

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 var EmailTemplate = require('email-templates').EmailTemplate;
 var path = require('path');
+var Q = require('q');
 
-
-var render = function (name, options, cb) {
+var render = function (name, options) {
+  var defer = new Q.defer();
   var templateDir = path.join(__dirname, 'templates', name);
 
   var element = new EmailTemplate(templateDir, {juiceOptions: {
     preserveMediaQueries: false,
-    removeStyleTags: false
+    removeStyleTags: true
   }});
 
-  element.render(options, cb);
+  element.render(options, function (err, result){
+    if (err) {
+      defer.reject(err);
+    } else {
+      defer.resolve(result);
+    }
+  });
+
+  return defer.promise;
 };
 
 
 var headerOptions = {title: 'Joe'};
 
-var buttonOptions = {action: 'sad', title: 'Example action'};
+var buttonOptions = {action: 'http://www.google.com', title: 'Example action'};
 
 var paragraphOptions = {content: 'hello world'};
 
 var header, layout, button, paragraph;
 
-render('button', buttonOptions, function (err, results) {
+render('button', buttonOptions)
+.then(function (results) {
   button = results.html;
-  //console.log(err, results);
-
-  render('header', headerOptions, function (err, results) {
-    //console.log(err, results);
-    header = results.html;
-
-    render('paragraph', paragraphOptions, function (err, results) {
-      //console.log(err, results);
-
-      paragraph = results.html;
-
-      var layoutOptions = {content: header + paragraph + button};
-
-      //console.log('header', header);
-
-      render('layout', layoutOptions, function (err, results) {
-        console.log(results.html);
-      });
-
-    });
-  });
+  return render('header', headerOptions);
+})
+.then(function (results) {
+  header = results.html;
+  paragraphOptions.content += button;
+  paragraphOptions.content += 'hello world';
+  return render('paragraph', paragraphOptions); 
+})
+.then(function (results) {
+  paragraph = results.html;
+  var layoutOptions = {content: header + paragraph + button + button + button + button};
+  return render('layout', layoutOptions); 
+})
+.then(function (results) {
+  console.log(results.html);
+})
+.fail(function (err) {
+  console.log(err);
 });
