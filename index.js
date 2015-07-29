e5e884c9358befe05e042c4d2e12c8b73d31fbce
antirek <serge.dmitriev@gmail.com>
2015-07-29 10:58:29 +0700

    add button compose

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var Container = require('./lib/compose/container');
 var Row = require('./lib/compose/row');
 var Column = require('./lib/compose/column');
 
+var Button = require('./lib/compose/button');
 
-var mail = new Mail({background: '#ddd'});
+var mail = new Mail({background: '#fff'});
 
 var container1 = new Container({background: '#fff'});
 var container2 = new Container({background: '#aaa'});
@@ -18,10 +19,9 @@ var row2 = new Row();
 var row3 = new Row();
 var row4 = new Row();
 
-
 var column1 = new Column({content: 'hello', cssClass: 'eight', extendedCssClass: 'text-pad panel'});
-var column2 = new Column({cssClass: 'eight', extendedCssClass: 'text-pad'});
-var column3 = new Column({cssClass: 'four', extendedCssClass: 'panel'});
+var column2 = new Column({cssClass: 'eight'});
+var column3 = new Column({cssClass: 'four', extendedCssClass: 'text-pad panel', last: 'last'});
 var column4 = new Column({content: 'world', cssClass: 'one'});
 
 
@@ -33,13 +33,18 @@ column3.addContent('<ul><li>Dorem lorem ipsum</li></ul>');
 
 column2.addContent('<img src="meelo.png">');
 
+var button = new Button({action:'http://www.google.com', title: 'Google', cssClass: 'secondary'});
+column2.addContent(button);
+
 row1.addColumn(column2);
 row1.addColumn(column3);
 
 /*
 row2.addColumn(column3);
-row2.addColumn(column2);
+row2.addColumn(column3);
+row2.addColumn(column3);
 
+/*
 row3.addColumn(column1);
 row3.addColumn(column3);
 row3.addColumn(column3);
@@ -50,7 +55,7 @@ row4.addColumn(column2);
 */
 
 container1.addRow(row1);
-container1.addRow(row1);
+container1.addRow(row2);
 
 container2.addRow(row1);
 container2.addRow(row1);
