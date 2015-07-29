b162daa79229451984ca20c9df32e4d9409130f1
antirek <serge.dmitriev@gmail.com>
2015-07-29 15:50:50 +0700

    change compose mail

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-var Renderer = require('./lib/renderer');
-var juice = require('juice');
-var q = new Renderer();
-
-var Mail = require('./lib/compose/mail');
-var Container = require('./lib/compose/container');
-var Row = require('./lib/compose/row');
-var Column = require('./lib/compose/column');
-
-var Button = require('./lib/compose/button');
+var Mail = require('./lib/bobo');
 
 var mail = new Mail({background: '#fff'});
 
-var container1 = new Container({background: '#fff'});
-var container2 = new Container({background: '#aaa'});
+var container1 = mail.container({background: '#fff'});
+var container2 = mail.container({background: '#aaa'});
 
-var row1 = new Row();
-var row2 = new Row();
-var row3 = new Row();
-var row4 = new Row();
+var row1 = mail.row();
+var row2 = mail.row();
+var row3 = mail.row();
+var row4 = mail.row();
 
-var column1 = new Column({content: 'hello', cssClass: 'eight', extendedCssClass: 'text-pad panel'});
-var column2 = new Column({cssClass: 'eight'});
-var column3 = new Column({cssClass: 'four', extendedCssClass: 'panel', last: 'last'});
-var column4 = new Column({content: 'world', cssClass: 'one'});
+var column1 = mail.column({content: 'hello', cssClass: 'eight', extendedCssClass: 'text-pad panel'});
+var column2 = mail.column({cssClass: 'eight'});
+var column3 = mail.column({cssClass: 'four', extendedCssClass: 'panel', last: 'last'});
+var column4 = mail.column({content: 'world', cssClass: 'one'});
 
 
 column2.addContent('<h1>Lopata</h1>');
@@ -33,7 +24,7 @@ column3.addContent('<ul><li>Dorem lorem ipsum</li></ul>');
 
 column2.addContent('<img src="meelo.png">');
 
-var button = new Button({action: 'http://www.google.com', title: 'Google', cssClass: 'secondary round'});
+var button = mail.button({action: 'http://www.google.com', title: 'Google', cssClass: 'secondary round'});
 column2.addContent(button);
 
 row1.addColumn(column2);
@@ -63,8 +54,6 @@ container2.addRow(row1);
 mail.addContainer(container1);
 mail.addContainer(container2);
 
-var qw = mail.build(q);
-
-qw = juice(qw);
+var qw = mail.build();
 
 console.log(qw);
