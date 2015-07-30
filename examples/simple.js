d3fce05bb047274836e7aba80e4f873557d526d0
antirek <serge.dmitriev@gmail.com>
2015-07-30 11:06:18 +0700

    column.addContent accept jade templates

--- examples/simple.js
+++ examples/simple.js
@@ -6,7 +6,7 @@ var header = function () {
   var row = mail.row();
   var column = mail.column({cssClass: 'twelve', extendedCssClass: 'text-pad'});
 
-  column.addContent('<h1>Good day, John</h1>');
+  column.addContent('h1 Good day, Vasya');
 
   row.addColumn(column);
   return row;
@@ -14,9 +14,14 @@ var header = function () {
 
 var content = function () {
   var row = mail.row();
-  var column = mail.column({cssClass: 'eight', extendedCssClass: 'text-pad'});
+  var column = mail.column({cssClass: 'eight', extendedCssClass: 'text-pad', offset: 'offset-by-two'});
 
-  column.addContent('<p>New updates are available for you!</p>');
+  var arr = [
+    'h3 New updates are available for you!',
+    'p All inclusive versions and updates you can download today.'
+  ];
+
+  column.addContent(arr);
   column.addContent(mail.button({action: 'http://www.google.com/', title:'Get it!', cssClass:'primary round'}));
   column.addContent('<p>&nbsp;</p>');  
   column.addContent('<img src="http://yastatic.net/morda-logo/i/arrow2/logo_simple.svg">');
@@ -46,5 +51,38 @@ container.addRow(footer());
 mail.addContainer(container);
 
 var html = mail.build();
+
+
+/*
+
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    
+});
+
+// NB! No need to recreate the transporter object. You can use
+// the same transporter object for all e-mails
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    to: 'serge.dmitriev@gmail.com', // list of receivers
+    subject: 'Hello 2', // Subject line
+    //text: , // plaintext body
+    html: html // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+
+});
+
+*/
 
 console.log(html);
