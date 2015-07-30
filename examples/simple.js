d6a20d3b281d404ca8d184f97aee018b7676d5d2
antirek <serge.dmitriev@gmail.com>
2015-07-30 13:54:13 +0700

    fix example

--- examples/simple.js
+++ examples/simple.js
@@ -1,10 +1,10 @@
-var Mail = require('../index');
+var Meelo = require('../index');
 
-var mail = new Mail({background: '#ddd'});
+var meelo = new Meelo({background: '#ddd'});
 
 var header = function () {
-  var row = mail.row();
-  var column = mail.column({cssClass: 'twelve', extendedCssClass: 'text-pad'});
+  var row = meelo.row();
+  var column = meelo.column({cssClass: 'twelve', extendedCssClass: 'text-pad'});
 
   column.addContent('h1 Good day, Vasya');
 
@@ -13,10 +13,10 @@ var header = function () {
 };
 
 var content = function () {
-  var row = mail.row();
+  var row = meelo.row();
 
-  var column1 = mail.column({content: '&nbsp;', cssClass: 'two', extendedCssClass: 'text-pad'});
-  var column = mail.column({cssClass: 'eight', extendedCssClass: 'text-pad'});
+  var column1 = meelo.column({content: '&nbsp;', cssClass: 'two', extendedCssClass: 'text-pad'});
+  var column = meelo.column({cssClass: 'eight', extendedCssClass: 'text-pad'});
 
   var arr = [
     'h3 New updates are available for you!',
@@ -24,8 +24,8 @@ var content = function () {
   ];
 
   column.addContent(arr);
-  column.addContent(mail.button({action: 'http://www.google.com/', title:'Get it!', cssClass:'primary round'}));
-  column.addContent('<p>&nbsp;</p>');  
+  column.addContent(meelo.button({action: 'http://www.google.com/', title:'Get it!', cssClass:'primary round'}));
+  column.addContent('br');  
   column.addContent('<img src="http://images5.fanpop.com/image/photos/31000000/Meelo-avatar-the-legend-of-korra-31027849-500-428.png">');
 
   row.addColumn(column1);
@@ -34,8 +34,8 @@ var content = function () {
 };
 
 var footer = function () {
-  var row = mail.row();
-  var column = mail.column({cssClass: 'twelve', extendedCssClass: 'text-pad'});
+  var row = meelo.row();
+  var column = meelo.column({cssClass: 'twelve', extendedCssClass: 'text-pad'});
 
   column.addContent('<h5>Contact us</h5>');
   column.addContent('<p>Phone: 8 391 2400000</p>');
@@ -44,46 +44,12 @@ var footer = function () {
   return row;
 };
 
-
-var container = mail.container({background: '#fff'});
+var container = meelo.container({background: '#fff'});
 
 container.addRow(header());
 container.addRow(content());
 container.addRow(footer());
 
-mail.addContainer(container);
+meelo.addContainer(container);
 
-var html = mail.build();
-
-
-
-
-var nodemailer = require('nodemailer');
-
-// create reusable transporter object using SMTP transport
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    
-});
-
-// NB! No need to recreate the transporter object. You can use
-// the same transporter object for all e-mails
-
-// setup e-mail data with unicode symbols
-var mailOptions = {
-    to: 'serge.dmitriev@gmail.com', // list of receivers
-    subject: 'Hello 2', // Subject line
-    //text: , // plaintext body
-    html: html // html body
-};
-
-// send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    //console.log('Message sent: ' + info.response);
-
-});
-
-console.log(html);
+var html = meelo.build();
